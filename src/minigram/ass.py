f0e52ec434c02554a87e122ccbcf929f94dd12a2
Grigory Bakunov <bobuk@users.noreply.github.com>
2024-04-03 13:05:40 +0300

    remove unneeded variable

--- src/minigram/ass.py
+++ src/minigram/ass.py
@@ -6,18 +6,17 @@ def ass(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         signature = inspect.signature(func).parameters
-        kargs = kwargs
         try:
             loop = asyncio.get_running_loop()
             if 'is_async' in signature:
                 kargs['is_async'] = True
-            return func(*args, **kargs)
+            return func(*args, **kwargs)
         except RuntimeError:
             if 'is_async' in signature:
                 kargs['is_async'] = False
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            result = loop.run_until_complete(func(*args, **kargs))
+            result = loop.run_until_complete(func(*args, **kwargs))
             loop.close()
             return result
 
